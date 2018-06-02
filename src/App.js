8f61d7b9e3a5ce1402e4eccf0dd722d47ea98ec6
김성민 <ignocide@gmail.com>
2018-06-02 16:45:07 +0900

    readme작성, 월 표시 잘못 되는 문제 수정, 리펙토링

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Number from './componenets/Number'
 import './App.scss'
 
 class App extends Component {
@@ -14,16 +15,17 @@ class App extends Component {
   }
 
   mask = (mask,value) => {
-    let str = Math.pow(10,mask.length) + value +''
+    let str = Math.pow(10,mask.length) + value + ''
     str = str.split('')
     str.shift()
 
     return str
   }
+  
   render () {
     let {date} = this.state
     let year = this.mask('xxxx',date.getFullYear())
-    let month = this.mask('xx',date.getMonth())
+    let month = this.mask('xx',date.getMonth() + 1)
     let day = this.mask('xx',date.getDate())
     let hour = this.mask('xx',date.getHours())
     let min = this.mask('xx',date.getMinutes())
@@ -55,62 +57,9 @@ class App extends Component {
             <Number number={sec[1]} />
           </div>
         </div>
-        <div className={'container'}>
-          <div className='top-half red' />
-          <div className='bottom-half green' id='animation' />
-        </div>
       </div>
     )
   }
 }
 
-class Number extends Component {
-  constructor () {
-    super()
-    this.state = {
-      before: null,
-      run: false
-    }
-  }
-
-  getSnapshotBeforeUpdate(prevProps,prevState){
-    if(this.props.number !== prevProps.number){
-      this.setState({
-        run: true
-      })
-    }
-
-    return null
-  }
-
-  static getDerivedStateFromProps(props){
-    let before = props.number -1
-
-    if(before < 0){
-      before = 9
-    }
-    return {
-      before
-    }
-  }
-
-  endAnimation = () => {
-    this.setState({
-      run: false
-    })
-  }
-
-  render () {
-    let {number} = this.props
-    let after = number
-    let {before,run} = this.state
-
-    return <div className='number'>
-      <span className='before-top' id={run ? 'fold' : ''} onAnimationEnd={this.endAnimation}><span className={'number-mask'}>{before}</span></span>
-      <span className='before-bottom'><span className={'number-mask'}>{before}</span></span>
-      <span className='after-top'><span className={'number-mask'}>{after}</span></span>
-      <span className='after-bottom' id={run ? 'fold-inv': ''} onAnimationEnd={this.endAnimation}><span className={'number-mask'}>{after}</span></span>
-    </div>
-  }
-}
 export default App
---
+++ src/componenets/Number/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import './index.scss'
+
+class Number extends Component {
+  constructor () {
+    super()
+    this.state = {
+      before: null,
+      run: false
+    }
+  }
+
+  getSnapshotBeforeUpdate(prevProps,prevState){
+    if(this.props.number !== prevProps.number){
+      this.setState({
+        run: true
+      })
+    }
+
+    return null
+  }
+
+  static getDerivedStateFromProps(props){
+    let before = props.number -1
+
+    if(before < 0){
+      before = 9
+    }
+    return {
+      before
+    }
+  }
+
+  endAnimation = () => {
+    this.setState({
+      run: false
+    })
+  }
+
+  render () {
+    let {number} = this.props
+    let after = number
+    let {before,run} = this.state
+
+    return <div className='number'>
+      <span className='before-top' id={run ? 'fold' : ''} onAnimationEnd={this.endAnimation}><span className={'number-mask'}>{before}</span></span>
+      <span className='before-bottom'><span className={'number-mask'}>{before}</span></span>
+      <span className='after-top'><span className={'number-mask'}>{after}</span></span>
+      <span className='after-bottom' id={run ? 'fold-inv': ''} onAnimationEnd={this.endAnimation}><span className={'number-mask'}>{after}</span></span>
+    </div>
+  }
+}
+
+export default Number
